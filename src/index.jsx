cacddfbe4c2de9b8497296c219c265b3a76880de
Alain Noel <alain.noel34@gmail.com>
2022-08-10 20:51:30 +0200

    route path ok

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Header from './components/Header'
+import Error from './components/Error'
+import Freelances from './pages/Freelances'
+import Results from './pages/Results'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/survey">
-        <Survey />
-      </Route>
+      <Header />
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+        <Route path="/freelances">
+          <Freelances />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Routes>
     </Router>
-
   </React.StrictMode>,
-  document.getElementById('root')
-);
+document.getElementById('root')
+)
