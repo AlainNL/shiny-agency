f5bfbd38d1d07661e27ac282ffdbc84da7761e13
Alain Noel <alain.noel34@gmail.com>
2022-08-16 01:47:00 +0200

    css en cours

--- src/index.jsx
+++ src/index.jsx
@@ -7,13 +7,20 @@ import Header from './components/Header';
 import Error from './components/Error';
 import Freelances from './pages/Freelances';
 import Results from './pages/Results';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 const root = ReactDOM.createRoot(
   document.getElementById("root")
 );
 root.render(
   <React.StrictMode>
       <BrowserRouter>
+        <GlobalStyle />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
