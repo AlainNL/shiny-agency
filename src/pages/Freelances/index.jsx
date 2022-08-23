df956d60c5da79b33918779895f26e050ded9c47
Alain Noel <alain.noel34@gmail.com>
2022-08-23 21:07:22 +0200

    API en cours

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,10 @@
-import DefaultPicture from '../../assets/profile.png'
-import Card from '../../components/Card'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
+import Card from '../../components/Card'
 
 const freelanceProfiles = [
     {
@@ -40,15 +43,50 @@ const PageSubtitle = styled.h2`
     padding-bottom: 30px;
 `
 
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 function Freelances() {
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [freelancersList, setFreelancesList] = useState ([])
+
+  useEffect(() => {
+    async function fetchFreelances() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:3000/freelances`)
+        const { freelancersList } = await response.json()
+        setFreelancesList(freelancersList)
+      } catch (err) {
+        console.log('===== error =====', err)
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchFreelances()
+  }, [])
+
+  if (error) {
+    return <span> Oups il y a eu un problème</span>
+  }
+
   return (
     <div>
         <PageTitle>Trouvez votre prestataire</PageTitle>
         <PageSubtitle>
           Chez Shiny, nous vous trouvons les meilleurs profils.
         </PageSubtitle>
+        {isDataLoading ? (
+          <LoaderWrapper>
+            <Loader />
+          </LoaderWrapper>
+        ) : (
         <CardsContainer>
-        {freelanceProfiles.map((profile, index) => (
+        {freelancersList.map((profile, index) => (
           <Card
           key={`${profile.name}-${index}`}
               label={profile.jobTitle}
@@ -57,6 +95,7 @@ function Freelances() {
           />
         ))}
         </CardsContainer>
+        )}
     </div>
   )
 }
