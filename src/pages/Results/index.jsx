33b9fef75e4cbcbf1d4d427ed430cdd989f05c29
Alain Noel <alain.noel34@gmail.com>
2022-09-13 18:51:27 +0200

    page results hook ok

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -1,15 +1,116 @@
 import { useContext } from 'react'
 import { SurveyContext } from '../../utils/context'
-import colors from '../utils/style/colors'
+import colors from '../../utils/style/colors'
+import styled from 'styled-components'
+import { useFetch, useTheme } from '../../utils/hooks'
+import { StyledLink, Loader } from '../../utils//style/Atoms'
+
+
+const ResultsContainer= styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 60px 90px;
+  padding 30px;
+  background-color: ${({ theme }) =>
+    theme = 'light' ? colors.backgroundLight : colors.backgroundDark}
+`
+
+const ResultsTitle = styled.h2`
+  color: ${({ theme }) => (
+    theme === 'light' ? '#00000' : 'fffff')}
+  font-weight: bold;
+  font-size: 28px;
+  max-width: 60%;
+  text-align: center;
+  & > span {
+    padding-left: 10px;
+  }
+`
+
+const DescriptionWrapper = styled.div`
+  padding: 60px;
+`
+const JobTitle = styled.span`
+  color: ${({ theme }) =>
+    theme = 'light' ? colors.primary : colors.backgroundLight}
+  text-transform: capitalize;
+`
+
+const JobDescription = styled.div`
+  font-size: 18px;
+  & > p {
+    color: ${({ theme }) => (theme === 'light' ? colors.secondary : 'fffff')};
+    margin-block-start: 5px;
+  }
+  & > span {
+    font-size: 20px;
+  }
+`
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+function formatFetchParams(answers) {
+  const answerNumbers = Object.keys(answers)
+
+  return answerNumbers.reduce((previousParams, answerNumber, index) => {
+    const isFirstParam = index === 0
+    const separator = isFirstParam ? '' : '&'
+    return `${previousParams}${separator}a${answerNumber}`
+  }, '')
+}
 
 function Results() {
+  const { theme } = useTheme()
   const { answers } = useContext(SurveyContext)
-  console.log(answers)
+  const fetchParams = formatFetchParams(answers)
 
-  return (
-    <div>
-      <h1>Résultats</h1>
-    </div>
+  const {data, isLoading, error } = useFetch(`http://localhost:8000/results?${fetchParams}`)
+
+  if (error) {
+    return <span> Oups, il y a un problème</span>
+  }
+
+  const resultsData = data?.resultsData
+
+  return isLoading ? (
+    <LoaderWrapper>
+      <Loader />
+    </LoaderWrapper>
+  ) : (
+    <ResultsContainer theme={theme}>
+      <ResultsTitle theme={theme}>
+        Les compétences dont vous avez besoin :
+        {resultsData &&
+          resultsData.map((result, index) => (
+            <JobTitle
+              key={`result-title-${index}-${result.title}`}
+              theme={theme}
+            >
+              {result.title}
+              {index === resultsData.length - 1 ? '' : ','}
+            </JobTitle>
+        ))}
+      </ResultsTitle>
+      <StyledLink $isFullLink to="/freelances">
+        Découvrez nos profils
+      </StyledLink>
+      <DescriptionWrapper>
+        {resultsData &&
+          resultsData.map((result, index) => (
+            <JobDescription
+              theme={theme}
+              key={`result-detail-${index}-${result.title}`}
+          >
+            <JobTitle theme={theme}>{result.title}</JobTitle>
+            <p>{result.description}</p>
+          </JobDescription>
+          ))}
+      </DescriptionWrapper>
+    </ResultsContainer>
   )
 }
 
