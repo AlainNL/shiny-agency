f86da509d6a08e9d3eeaa02efc08e44088af5505
Alain Noel <alain.noel34@gmail.com>
2022-09-14 17:30:24 +0200

    test page result ok

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -53,6 +53,23 @@ const LoaderWrapper = styled.div`
   justify-content: center;
 `
 
+export function formatJobList(title, listLength, index) {
+    if (index === listLength -1) {
+        return title
+    }
+    return `${title},`
+}
+
+export function formatQueryParams(answers) {
+  const answerNumbers = Object.keys(answers)
+
+  return answerNumbers.reduce((previousParams, answerNumber, index) => {
+    const isFirstParam = index === 0
+    const separator = isFirstParam ? '' : '&'
+    return `${previousParams}${separator}a${answerNumber}=${answers[answerNumber]}`
+  }, '')
+}
+
 function formatFetchParams(answers) {
   const answerNumbers = Object.keys(answers)
 
@@ -91,7 +108,7 @@ function Results() {
               theme={theme}
             >
               {result.title}
-              {index === resultsData.length - 1 ? '' : ','}
+              {formatJobList(result.title, resultsData.length, index)}
             </JobTitle>
         ))}
       </ResultsTitle>
