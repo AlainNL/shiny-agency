5e691fcb6b13ebcf6bbd929d942ec4765e118863
Alain Noel <alain.noel34@gmail.com>
2022-10-07 18:44:20 +0200

    add reducer / freelancer in store.js

---
+++ src/features/freelances.js
@@ -0,0 +1,58 @@
+import produce from 'immer'
+
+const initialState = {
+
+  status: 'void',
+  date: null,
+  error: null,
+}
+
+const FETCHING = 'freelances/fetching'
+const RESOLVED = 'freelances/resolved'
+const REJECTED = 'freelances/rejected'
+
+const freelancesFetching = () => ({ type: FETCHING })
+const freelancesResolved = (data) => ({ type: RESOLVED, payload: data })
+const freelancesRejected = (error) => ({ type: REJECTED, payload: error })
+
+export default function freelancesReducer(state = initialState,action) {
+    return produce(state, (draft) => {
+      switch (action.type) {
+        case FETCHING: {
+          if (draft.status === 'void') {
+            draft.status = 'pending'
+            return
+          }
+          if (draft.status === 'rejected') {
+            draft.error = null
+            draft.status = 'pending'
+            return
+          }
+          if (draft.status === 'resolved') {
+            draft.status = 'updating'
+            return
+          }
+          return
+        }
+        case RESOLVED: {
+          if (draft.status === 'pending' || draft.status === 'updating') {
+            draft.data = action.payload
+            draft.status = 'resolved'
+            return
+          }
+          return
+        }
+        case REJECTED: {
+          if (draft.status === 'pending' || draft.status === 'updating') {
+            draft.status = 'rejected'
+            draft.error = action.payload
+            draft.data = null
+            return
+        }
+          return
+      }
+      default:
+          return
+    }
+  })
+}
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,13 @@
 import { combineReducers, legacy_createStore as createStore } from "redux";
 import themeReducer from '../features/theme.js'
+import freelancesReducer from "../features/freelances.js";
 
 
 const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const reducer = combineReducers({
   theme: themeReducer,
+  freelances: freelancesReducer,
 })
 
 const store = createStore(reducer, reduxDevtools)
