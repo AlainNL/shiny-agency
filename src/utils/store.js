b0bb817b04760fd21b4868740fdbce76ac3ffe79
Alain Noel <alain.noel34@gmail.com>
2022-10-10 15:58:21 +0200

    feature survey: done

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,8 @@
 import { combineReducers, legacy_createStore as createStore } from "redux"
 import themeReducer from '../features/theme.js'
 import freelancesReducer from "../features/freelances.js"
-import survey from '../features/survey.js'
-import freelance from '../features/freelance.js'
+import surveyReducer from '../features/survey.js'
+import freelanceReducer from '../features/freelance.js'
 
 
 const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -11,7 +11,7 @@ const reducer = combineReducers({
   theme: themeReducer,
   freelances: freelancesReducer,
   survey: surveyReducer,
-  freelance: reelanceReducer,
+  freelance: freelanceReducer,
 })
 
 const store = createStore(reducer, reduxDevtools)
