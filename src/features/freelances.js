83a38251da4f999eaaee2cb44d6c8d8ff21e85ba
Alain Noel <alain.noel34@gmail.com>
2022-10-10 16:15:59 +0200

    feature freelances async:ok

--- src/features/freelances.js
+++ src/features/freelances.js
@@ -16,6 +16,21 @@ const freelancesFetching = () => ({ type: FETCHING })
 const freelancesResolved = (data) => ({ type: RESOLVED, payload: data })
 const freelancesRejected = (error) => ({ type: REJECTED, payload: error })
 
+export async function fetchOrUpdateFreelances(store) {
+  const status = selectFreelances(store.getState()).status
+  if (status === 'pending' || status === 'updating') {
+    return
+  }
+  store.dispatch(freelancesFetching())
+  try {
+    const response = await fetch('http://localhost:8000/freelances')
+    const data = await response.json()
+    store.dispatch(freelancesResolved(data))
+  } catch (error) {
+    store.dispatch(freelancesRejected(error))
+  }
+}
+
 export default function freelancesReducer(state = initialState,action) {
     return produce(state, (draft) => {
       switch (action.type) {
