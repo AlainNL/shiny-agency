7e189c4f23ea0e439a9e9cba72cf0362ad352715
Alain Noel <alain.noel34@gmail.com>
2022-10-10 14:27:29 +0200

    feature survey en cours

--- src/features/freelances.js
+++ src/features/freelances.js
@@ -1,4 +1,5 @@
 import produce from 'immer'
+import { selectFreelances } from '../utils/selectors'
 
 const initialState = {
 
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,6 +1,8 @@
-import { combineReducers, legacy_createStore as createStore } from "redux";
+import { combineReducers, legacy_createStore as createStore } from "redux"
 import themeReducer from '../features/theme.js'
-import freelancesReducer from "../features/freelances.js";
+import freelancesReducer from "../features/freelances.js"
+import survey from '../features/survey.js'
+import freelance from '../features/freelance.js'
 
 
 const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -8,6 +10,8 @@ const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVT
 const reducer = combineReducers({
   theme: themeReducer,
   freelances: freelancesReducer,
+  survey: surveyReducer,
+  freelance: reelanceReducer,
 })
 
 const store = createStore(reducer, reduxDevtools)
